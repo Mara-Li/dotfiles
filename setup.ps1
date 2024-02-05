1e9de0c4efaa0aa5811593f42e45b89754f80804
Mara-Li <lili.simonetti@outlook.fr>
2024-02-05 11:08:18 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,2 @@
-winget install Microsoft.VisualStudioCode Starship.Starship Chocolatey.Chocolatey SomePythonThings.WingetUIStore junegunn.fzf Microsoft.PowerToys Microsoft.PowerShell gerardog.gsudo Git.Git GitHub.cli JanDeDobbeleer.OhMyPosh
+winget install Microsoft.VisualStudioCode Starship.Starship Chocolatey.Chocolatey SomePythonThings.WingetUIStore junegunn.fzf Microsoft.PowerToys Microsoft.PowerShell gerardog.gsudo Git.Git GitHub.cli JanDeDobbeleer.OhMyPosh sharkdp.fd
 Install-Module -Name PSFzf -Scope CurrentUser
