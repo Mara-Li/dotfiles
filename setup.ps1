a633e06512bd33ccdd38ba6081fd79857e4f0d23
Mara-Li <lili.simonetti@outlook.fr>
2024-02-05 14:19:10 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,4 @@
 winget install Microsoft.VisualStudioCode Starship.Starship Chocolatey.Chocolatey SomePythonThings.WingetUIStore junegunn.fzf Microsoft.PowerToys Microsoft.PowerShell gerardog.gsudo Git.Git GitHub.cli JanDeDobbeleer.OhMyPosh sharkdp.fd
+winget install --id=Discord.Discord  -e
+winget install --id=Vendicated.Vencord.PTB  -e
 Install-Module -Name PSFzf -Scope CurrentUser
